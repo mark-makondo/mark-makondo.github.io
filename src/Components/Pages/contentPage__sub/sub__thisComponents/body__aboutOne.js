8f175c8c466d7e17f53da6af29023dd6834f9be1
Mark Makondo <markalbert.makondo@gmail.com>
2021-01-05 18:46:15 +0800

    css adjusted

--- src/Components/Pages/contentPage__sub/sub__thisComponents/body__aboutOne.js
+++ src/Components/Pages/contentPage__sub/sub__thisComponents/body__aboutOne.js
@@ -3,13 +3,14 @@ import React from 'react';
 const body__aboutOne = () =>{
     return(
         <div className="body__aboutOne flex-column-center"id="getStarted">
-            <span className="body__aboutOne__title font-title">Web Developer</span>
-            <hr className="custom-hr-about"/>
-            <span className="body__aboutOne__desc font-content">
-               <q> Testing leads to failure, and failure leads to understanding. </q>
-               <figcaption><cite>-Burt Rutan </cite></figcaption>
-            </span>
-            
+            <div className="aboutOne__info">
+                <span className="body__aboutOne__title font-title">Web Developer</span>
+                <hr className="custom-hr-about"/>
+                <span className="body__aboutOne__desc font-content">
+                <q> Testing leads to failure, and failure leads to understanding. </q>
+                <figcaption><cite>-Burt Rutan </cite></figcaption>
+                </span>
+            </div>
             <div className="btn__getStarted font-title" >
                 <a href="">Get Started</a>
                 <i className="fas fa-arrow-right btn__arrow"></i>
