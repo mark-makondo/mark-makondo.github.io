9f0619d562c63f84accdef03a8a88c7cc39ddaf5
Mark Makondo <markalbert.makondo@gmail.com>
2021-02-23 11:35:05 +0800

    added new project

--- src/components/p-item_holder/p-item_holder-container.js
+++ src/components/p-item_holder/p-item_holder-container.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ProjectHolderUi from './p-item_holder.js';
 
 // assets
-import PortfolioOne from  '../../assets/img/img-projects/old-portfolio.png';
-import PortfolioTwo from  '../../assets/img/img-projects/cha-beauty.png';
-import PortfolioThree from  '../../assets/img/img-projects/github-api-fetch.png';
+import ProjectOne from  '../../assets/img/img-projects/old-portfolio.png';
+import ProjectTwo from  '../../assets/img/img-projects/cha-beauty.png';
+import ProjectThree from  '../../assets/img/img-projects/github-api-fetch.png';
+import ProjectFour from  '../../assets/img/img-projects/github-api-fetch.png';
 
 const ProjectHolderContianer = () => {
     let projects = [
-        {"id": 0, "image": `${PortfolioOne}`, "title": "Old Portfolio", "src": "https://mark-makondo.github.io/Portfolio-v2/"},
-        {"id": 1, "image": `${PortfolioTwo}`, "title": "Cha Portfolio", "src": "https://mark-makondo.github.io/cha-website/"},
-        {"id": 2, "image": `${PortfolioThree}`, "title": "Github Api Fetch", "src": "https://mark-makondo.github.io/github-api-web/"}
+        {"id": 0, "image": `${ProjectOne}`, "title": "Old Portfolio", "src": "https://mark-makondo.github.io/Portfolio-v2/"},
+        {"id": 1, "image": `${ProjectTwo}`, "title": "Cha Portfolio", "src": "https://mark-makondo.github.io/cha-website/"},
+        {"id": 2, "image": `${ProjectThree}`, "title": "Github Api Fetch", "src": "https://mark-makondo.github.io/github-api-web/"},
+        {"id": 3, "image": `${ProjectFour}`, "title": "Blog Post Manager", "src": "https://mark-makondo.github.io/blog-post-manager/"}
     ]
 
     return(
