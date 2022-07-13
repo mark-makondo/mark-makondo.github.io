bdb9494053dffe9298b72035d3db888876bb1d5e
Mark Makondo <markalbert.makondo@gmail.com>
2022-07-13 15:21:52 +0300

    Added new updates
    - changed node-sass to sass for updated sass codes.
    - added usefull scss codes for starter.
    
    Signed-off by Mark Makondo <markalbert.makondo@gmail.com>

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import Home from './home/Home';
+
+const App = () => {
+    return <div className="secondary-font">App</div>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './stylesheets/index.css';
-import App from './components/home/Home';
+import './main.css';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
